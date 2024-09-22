fc539f9ae38fcf0c63fe17fe2797e68f26dd7ff7
koheihida <koheihida04@gmail.com>
2024-09-23 00:51:00 +0900

    CSSの仮実装

--- reactstudy/src/App.jsx
+++ reactstudy/src/App.jsx
@@ -1,11 +1,17 @@
 export const App = () => {
+  // ボタン押下時の関数
   const onClickButton = () => {
     alert("動作確認")
   }
+  // pタグ用のCSS
+  const pStyle = {
+    fontSize : "50px",
+    color: "blue"
+  }
   return (
     <>
-    <h1>Hello World</h1>
-    <p>Digital Nature</p>
+    <h1 style={{color: "red" }}>Hello World</h1>
+    <p style={pStyle}>Digital Nature</p>
     <button onClick={onClickButton}>ボタン</button>
     </>
   ); 
