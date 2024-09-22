e2f2e1ba2e3f980a5437a79ba5bf9265dbf38d99
koheihida <koheihida04@gmail.com>
2024-09-23 01:10:20 +0900

    ColorfulMessageの仮実装

--- reactstudy/src/App.jsx
+++ reactstudy/src/App.jsx
@@ -1,17 +1,15 @@
+import { ColorfulMessage } from "./components/ColorfulMessage";
+
 export const App = () => {
   // ボタン押下時の関数
   const onClickButton = () => {
     alert("動作確認")
   }
-  // pタグ用のCSS
-  const pStyle = {
-    fontSize : "50px",
-    color: "blue"
-  }
+
   return (
     <>
     <h1 style={{color: "red" }}>Hello World</h1>
-    <p style={pStyle}>Digital Nature</p>
+    <ColorfulMessage />
     <button onClick={onClickButton}>ボタン</button>
     </>
   ); 
---
+++ reactstudy/src/components/ColorfulMessage.jsx
@@ -0,0 +1,10 @@
+export const ColorfulMessage = () => {
+    // pタグ用のCSS
+    const pStyle = {
+      fontSize : "50px",
+      color: "blue"
+    }
+    return (
+      <p style={pStyle}>Digital Nature</p>
+    )
+}
